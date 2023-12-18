0ccc8f86206e151e4d6cad1326b6e6b9cb02b3a8
Eric Bandholz <eric@sovrnty.com>
2023-12-18 16:10:15 -0600

    Update fetch-inventory.sh
    update search to only tacomas and 2023 & 2024

--- fetch-inventory.sh
+++ fetch-inventory.sh
@@ -17,8 +17,8 @@ do
       "pagesize": 300,
       "pagestart": 0,
       "filter": {
-          "year": [2022, 2023], 
-          "series": ["tacoma", "4runner", "tundra", "rav4"], 
+          "year": [2023, 2024], 
+          "series": ["tacoma"], 
           "dealers": ["'$i'"],
           "andfields": ["accessory", "packages", "dealer"]
         }
